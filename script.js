a42bc32e0ee884417fe058aea8cde71b73e45ac0
samina-shameem <samina.shameem.clt@gmail.com>
2025-01-02 11:49:18 +0100

    added wind and humudity.

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ const weatherDisplay = document.getElementById('weather-display');
 const API_KEY = '0cede45c2541d939f4ddbdc37801b8c7';//replace with API key
 
 form.addEventListener('submit', function (event) {
-    event.preventDefault();//prevent the form from refreshing the page
+    event.preventDefault(); // Prevent the form from refreshing the page
 
     const city = cityInput.value.trim();
-    if (city) {
-        getWeather(city);
-    } else {
-        weatherDisplay.innerHTML = '<p>Please enter a city name.</p>';
+    if (!city) {
+        weatherDisplay.innerHTML = '<p style="color: red;">Please enter a city name.</p>';
+        return;
     }
+
+    getWeather(city);
 });
 
 async function getWeather(city) {
+    weatherDisplay.innerHTML='<p>Loading....</p>';//show loading text
     try {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
@@ -27,16 +29,18 @@ async function getWeather(city) {
         const data=await response.json();
         displayWeather(data);
     }catch(error){
-         weatherDisplay.innerHTML=`<p>Error:${error.message}</p>`;
+         weatherDisplay.innerHTML=`<p style="color red;">Error:${error.message}</p>`;
     }
     
 }
 
 function displayWeather(data){
-    const{ name, main, weather }=data;
+    const{ name, main, weather, wind}=data;
     weatherDisplay.innerHTML=`
           <h2>${name}</h2>
         <p>Temperature: ${main.temp}°C</p>
         <p>Weather: ${weather[0].description}</p>
+        <p>Humidity:${main.humidity}%</p>
+        <p>Wind Speed:${window.speed}m/s</p>
     `;
 }
